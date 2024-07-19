cb1594f2ee0a88302987f3742c0ffd11d80b904b
ddusichka <78527291+ddusichka@users.noreply.github.com>
2024-07-19 14:07:52 -0400

    add admin functionality

--- matches/admin.py
+++ matches/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from matches.models import Match
+
 # Register your models here.
+admin.site.register(Match)
--- tracks/admin.py
+++ tracks/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from tracks.models import Track, User
+
 # Register your models here.
+admin.site.register(Track)
+admin.site.register(User)
